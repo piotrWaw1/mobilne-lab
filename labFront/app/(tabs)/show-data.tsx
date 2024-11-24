ea3a182ccae47aaf5e346d4573043fe9969e2f01
Piotr <piotrwc49@gmail.com>
2024-11-24 17:57:17 +0100

    storage delete

--- labFront/app/(tabs)/show-data.tsx
+++ labFront/app/(tabs)/show-data.tsx
@@ -3,28 +3,43 @@ import { getAllItems } from "@/components/utils/AsyncStorage";
 import { ThemedText } from "@/components/ThemedText";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import ThemedSaveAreaView from "@/components/ThemedSaveAreaView";
-import { View } from "react-native";
+import { Pressable, View } from "react-native";
 
 export default function ShowData() {
   const [data, setData] = useState({});
-  useEffect(() => {
-    const d = async () => {
-      // const keys = await AsyncStorage.getAllKeys()
-      // const items = await AsyncStorage.multiGet(keys);
-      const items = await getAllItems()
-      setData(items)
-      console.log(items)
-    }
 
-    d().then()
+  const init = async () => {
+    const items = await getAllItems()
+    setData(items)
+    console.log(items)
+  }
+
+  useEffect(() => {
+    init().then()
   }, [])
+
+  const deleteItem = async (itemKey: string) => {
+    try {
+      await AsyncStorage.removeItem(itemKey);
+      init().then()
+    } catch (error) {
+      console.error('Error removing item:', error);
+    }
+  }
+
   return (
     <ThemedSaveAreaView className="px-2">
       <ThemedText>Data show</ThemedText>
       <View className="flex gap-2 border p-2">
-      {Object.entries(data).map(([key, value]) => (
-        <ThemedText key={key}>{key}: {value}</ThemedText>
-      ))}
+        {Object.entries(data).map(([key, value]) => (
+          <View key={key}>
+            <ThemedText>{key}: {value}</ThemedText>
+            <Pressable onPress={() => deleteItem(key)} className="p-2 bg-red-500">
+              <ThemedText className="font-bold text-center" style={{ color: "white" }}>Delete</ThemedText>
+            </Pressable>
+          </View>
+
+        ))}
       </View>
     </ThemedSaveAreaView>
 
